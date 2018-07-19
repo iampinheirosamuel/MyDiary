4cc32a307b518edb69a58129446676fe9069ce7f
iampinheirosamuel <pinheirolaoluwa@gmail.com>
2018-07-19 12:28:33 +0100

    Created API endpoint POST /entries

--- app/index.js
+++ app/index.js
@@ -14,6 +14,26 @@ const router = express.Router();
 router.use((req, res, next) => { next(); });
 
 // Set Up Routes
+// create an entry for MyDiary
+const entries = new Set([]);
+router.route('/entries').post((req, res) => {
+        const today = new Date();
+        const entry = {};
+        if (typeof req.body.name === 'string' && typeof req.body.content === 'string') {
+            entry.name = req.body.name;
+            entry.content = req.body.content;
+            entry.created_at = today.toLocaleDateString('en-US', { year: 'numeric', month: 'long', day: 'numeric' });
+            entries.add(entry);
+            res.status(201).json({
+                entries: 'Entry added successfully',
+            });
+        } else {
+            return res.status(400).send({
+                Response: 'Bad Request',
+            }); // 400 Bad Request
+        }
+    });
+
 
 // Register Routes
 app.use('/api/v1', router);
