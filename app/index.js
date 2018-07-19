9d34445746b1e03e814af9b8dc2ab2ea11b7e0dc
iampinheirosamuel <pinheirolaoluwa@gmail.com>
2018-07-19 11:50:04 +0100

    API routes and middleware created

--- app/index.js
+++ app/index.js
@@ -6,6 +6,18 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json()); // parses json
 const port = process.env.PORT || 8080; // set port
+
+// Routes for API
+const router = express.Router();
+
+// middleware to use for all requests
+router.use((req, res, next) => { next(); });
+
+// Set Up Routes
+
+// Register Routes
+app.use('/api/v1', router);
+
 // Start the Server
 app.listen(port);
 console.log(`server started at: ${port}`);
