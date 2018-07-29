9a395f12e2f38a231bd12ecd711c13f0ae682355
iampinheirosamuel <pinheirolaoluwa@gmail.com>
2018-07-29 01:12:34 +0100

    chore(travis)refactor develop

--- src/models/model.js
+++ src/models/model.js
@@ -1,16 +1,3 @@
-import { Pool, Client } from 'pg';
-
-const pool = new Pool({
-  user: '',
-  database: '',
-  password: '',
-  host: '',
-  port: '',
-});
-
-pool.query('SELECT NOW()', (req, res) => {
-  console.log(req, res);
-});
 
 const data = [];
 export default data;
